f2b79d9c4f00599950050d1ba68071ef10db926a
scarpma <scarpma@gmail.com>
2024-06-12 13:01:26 +0200

    fixed automatically found venc being ignored

--- dicoms_to_vtk.py
+++ dicoms_to_vtk.py
@@ -31,6 +31,8 @@ args = parser.parse_args()
 #-----------------------------------------------------------------------------------------------------------------------
 ## Read dicom files and create numpy arrays
 data, meta = ut.read_acquisition(args.data_dir)
+if args.venc == (0,0,0):
+    args.venc = meta['venc']
 arrayData = ut.seriesData_to_arrayData(data, meta)
 
 #-----------------------------------------------------------------------------------------------------------------------
